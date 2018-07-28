2668c19c250ae8ed8c08136e17ca7ff140a37a27
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 02:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,20 @@ function BoardMember(name, homeState, training){
   this.name=name;
   this.homeState=homeState;
   this.training=training;
+  this.veto=function(){
+    return "No, I must disagree";
+  };
+  
+  this.approve=function(){
+    return "You can do that!";
+  };
+  
+  this.doCharity=function(){
+  return "I like to help people.";
+  };
+  
+  this.releasePressStatement=function(){
+    
+    
+  }
 }
